0f8f64a582388f9deb6a5c1417bf603a63d332df
blakpaint <hien.harlicious@gmail.com>
2018-04-01 20:19:46 +0700

    Day04

---
+++ TCP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("125.253.118.38"), 81);
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.SendTimeout = 1000;
+            client.ReceiveTimeout = 1000;
+
+            try
+            {
+                client.Connect(ipep);
+                Console.WriteLine(ipep.ToString() + "is opened.");
+                client.Close();
+            }
+            catch
+            {
+                Console.WriteLine(ipep.ToString() + "is closed.");
+            }
+        }
+    }
+}
---
+++ UDP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("125.253.118.38"), 443);
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            client.SendTimeout = 1000;
+            client.ReceiveTimeout = 1000;
+
+            try
+            {
+                client.Connect(ipep);
+                Console.WriteLine(ipep.ToString() + "is opened.");
+                client.Close();
+            }
+            catch
+            {
+                Console.WriteLine(ipep.ToString() + "is closed.");
+            }
+        }
+    }
+}
